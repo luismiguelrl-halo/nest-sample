84fcc1a84a92e46166eb58bf1285f98e7998f765
Luis Miguel Ruidias <luis.ruidias@halopowered.com>
2022-09-05 17:10:50 -0500

    First demo

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://luismiguelrl:Genbeta..14@cluster0.adt2d.mongodb.net/sample_training?retryWrites=true&w=majority',
+};
---
+++ src/items/items.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/items.interface';
+import { CreateItemDto } from './dto/create-item.dto';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  async findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
+  }
+}
